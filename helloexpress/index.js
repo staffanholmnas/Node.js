17b6752cbadb9a31a05cca52115e8b37248fa144
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-30 17:04:37 +0200

    do exercise 4.1

--- helloexpress/index.js
+++ helloexpress/index.js
@@ -4,8 +4,8 @@ const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
+app.get("/home/user/:name/:age", (req, res) => {
+  res.send(`Welcome ${req.params.name}, you're ${req.params.age} years old`);
 })
 
 app.listen(port, () => {
