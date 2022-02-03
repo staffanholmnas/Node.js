d9721afb1e74d70bdca7a8e4ae07b8f6d2e3b55e
staffanholmnas <staffan.holmnas@centria.fi>
2022-02-03 23:02:49 +0200

    begin ex 7_2

---
+++ mongocardb/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+//MongoDB connection
+const mongoose = require('mongoose');
+const mongoURL = 'mongodb+srv://mongoviope:tetra61@cluster0.hmnmj.mongodb.net/carsdb?retryWrites=true&w=majority';
+mongoose.connect(mongoURL, { useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
