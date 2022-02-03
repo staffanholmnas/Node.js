154e2d45d809451dc9ef2617f4ac9c5f9b9242f6
staffanholmnas <staffan.holmnas@centria.fi>
2022-02-03 21:49:11 +0200

    create mongodb connection

--- mongomoviedb/index.js
+++ mongomoviedb/index.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+// MongoDB connection
+const mongoose = require('mongoose');
+const mongoURL = 'mongodb+srv://mongoviope:tetra61@cluster0.hmnmj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoURL, { useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
 const port = 3000;
 
 app.listen(port, () => {
