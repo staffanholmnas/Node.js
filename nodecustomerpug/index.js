384c6aa991c1b48ecf4e1bbf4d5e25ee01fffc90
staffanholmnas <staffan.holmnas@centria.fi>
2022-02-05 17:20:54 +0200

    edit index

--- nodecustomerpug/index.js
+++ nodecustomerpug/index.js
@@ -1,23 +1,21 @@
-const express = require('express'); 
+const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const port = 3000;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const port = 3000;
-
 app.set('view engine', 'pug')
 
 let customers = [
-  {id: '1588323375416', firstname: 'John', lastname: 'Johnson', email: 'john@johnson.com', phone: '8233243'},
-  {id: '1588323375417', firstname: 'Mary', lastname: 'Smith', email: 'mary@smith.com', phone: '6654113'},
-  {id: '1588323375418', firstname: 'Peter', lastname: 'North', email: 'peter@north.com', phone: '901176'},
+  { id: '1588323375416', firstname: 'John', lastname: 'Johnson', email: 'john@johnson.com', phone: '8233243' },
+  { id: '1588323375417', firstname: 'Mary', lastname: 'Smith', email: 'mary@smith.com', phone: '6654113' },
+  { id: '1588323375418', firstname: 'Peter', lastname: 'North', email: 'peter@north.com', phone: '901176' },
 ]
 
 app.get("/", (req, res) => {
-  res.render("customerlist", {customers: customers});
+  res.render("customerlist", { customers: customers });
 })
 
 app.get("/addcustomer", (req, res) => {
@@ -25,7 +23,7 @@ app.get("/addcustomer", (req, res) => {
 })
 
 app.post("/addcustomer", (req, res) => {
-  const newCustomer = {id: new Date().now, firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, phone: req.body.phone};
+  const newCustomer = { id: new Date().now, firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, phone: req.body.phone };
   customers = [...customers, newCustomer];
   res.redirect("/");
 })
